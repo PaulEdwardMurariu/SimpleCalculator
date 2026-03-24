549d8797f4c50c77aff3ad284822ca9dd35a0674
PaulEdwardMurariu <paul.edward.murariu@gmail.com>
2026-03-23 22:59:20 -0400

    Add calculator code

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# This is a simple calculator program done in Python to learn the fundamentals
+
+# Prints welcoming message, options for calculator, & choosing an option to perform
+print("Hello there! Welcome to the Simple Calculator!")
+
+num1 = float(input("What is your first number?"))
+num2 = float(input("What is your second number?"))
+
+print("1 - Add")
+print("2- Subtract")
+print("3 - Multiply")
+print("4- Divide")
+
+choice = input("Choose an operation:")
+
+
+
+
+
+
+
+
+
+
+
+#num1 = input("What is your first number?")
+#num2 = input("What is your second number?")
+
