5ba34a5e42870178628449abf438b041cd9e6505
PaulEdwardMurariu <paul.edward.murariu@gmail.com>
2026-03-24 22:21:57 -0400

    Added operations logic, edge case error message, ability to print result

--- Calculator.py
+++ Calculator.py
@@ -3,26 +3,27 @@
 # Prints welcoming message, options for calculator, & choosing an option to perform
 print("Hello there! Welcome to the Simple Calculator!")
 
-num1 = float(input("What is your first number?"))
-num2 = float(input("What is your second number?"))
-
 print("1 - Add")
 print("2- Subtract")
 print("3 - Multiply")
 print("4- Divide")
 
-choice = input("Choose an operation:")
+choice = int(input("Choose an operation:"))
 
+if choice in [1,2,3,4]:
+    num1 = float(input("Enter your first number: "))
+    num2 = float(input("Enter your second number: "))
 
+    if choice == 1:
+        result = num1 + num2
+    elif choice == 2:
+        result = num1 - num2
+    elif choice == 3:
+        result = num1 * num2
+    elif choice == 4:
+        result = num1 / num2
 
+else:
+    print("Invalid operation entered")
 
-
-
-
-
-
-
-
-#num1 = input("What is your first number?")
-#num2 = input("What is your second number?")
-
+print(f"The result is {result}")
